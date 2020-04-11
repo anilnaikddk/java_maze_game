bdf17653eebf73a6fd378147a20e86f3ab683310
Jarvis <Jarvis@DESKTOP-LBBTDA4>
2020-04-11 20:55:01 +0530

    Initial Commit

---
+++ Maze/src/maze/KeyBoard.java
@@ -0,0 +1,19 @@
+package maze;
+
+public class KeyBoard {
+	public static final int LEFT = 37;
+	public static final int UP = 38;
+	public static final int RIGHT = 39;
+	public static final int DOWN = 40;
+
+	public static final int SPACE = 32;
+	public static final int ESC = 27;
+	public static final int ENTER = 10;
+	public static final int BACKSPPACE = 8;
+	
+	public static final int A = 'A';
+	public static final int S = 'S';
+	public static final int D = 'D';
+	public static final int W = 'W';
+	public static final int R = 'R';
+}
